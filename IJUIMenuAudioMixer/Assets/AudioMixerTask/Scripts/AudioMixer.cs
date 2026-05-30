07de0f04dd70f36766541d4add62e5ecc00819c4
Aram Margaryan <marg-aa@mail.ru>
2026-05-30 21:46:31 +0300

    added some changes in script AudioMixer

--- IJUIMenuAudioMixer/Assets/AudioMixerTask/Scripts/AudioMixer.cs
+++ IJUIMenuAudioMixer/Assets/AudioMixerTask/Scripts/AudioMixer.cs
@@ -5,12 +5,27 @@ using UnityEngine.Audio;
 public class AudioMixer : MonoBehaviour
 {
     private const string MasterVolume = "MasterVolume";
+    private const string BackgroundVolume = "BackgroundVolume";
 
     [SerializeField] private AudioMixerGroup _audioMixerGroup;
     [SerializeField] private Toggle _toggleOnOff;
+    [SerializeField] private AudioSource _audioSource;
+    [SerializeField] private Slider _slider;
+
+    private float _currentVolume;
 
     private float _muteSound = -80;
     private float _normalSound = 0;
+
+    private void Awake()
+    {
+        _currentVolume = _audioSource.volume;
+
+        _slider.value = _currentVolume;
+
+        _audioMixerGroup.audioMixer.SetFloat(BackgroundVolume,
+            Mathf.Log10(_currentVolume) * 20);
+    }
 
     private void OnEnable()
     {
@@ -21,7 +36,7 @@ public class AudioMixer : MonoBehaviour
     {
         _toggleOnOff.onValueChanged.RemoveListener(OnToggleOnOff);
     }
-    
+
     private void OnToggleOnOff(bool isOnSound)
     {
         if (isOnSound)
